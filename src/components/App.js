57e4b35b217f1e3ebd21e9ce898a2dd43e5cc25c
Kamila Matla-Tomczyk <ka1130@o2.pl>
2020-01-30 22:08:44 +0100

    Add basic drag and drop

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React from "react";
-import MyDropzone from "components/DD";
+import { useDropzone } from "react-dropzone";
 import styles from "./styles.module.scss";
+
+function Basic(props) {
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <section className="container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </section>
+  );
+}
 
 function App() {
   return (
     <div className={styles.wrapper}>
-      <MyDropzone />
+      <Basic />
     </div>
   );
 }
