773b88905bc2313987f739d660d8c459b29903e0
Kamila Matla-Tomczyk <ka1130@o2.pl>
2020-01-30 21:52:32 +0100

    Setup absolute paths

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import MyDropzone from "components/DD";
 import styles from "./styles.module.scss";
 
 function App() {
-  return <div className={styles.wrapper}>APP</div>;
+  return (
+    <div className={styles.wrapper}>
+      <MyDropzone />
+    </div>
+  );
 }
 
 export default App;
